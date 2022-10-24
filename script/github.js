3486b9ac3bb800d12876349d0159cd8b062ecfaa
Shubham18598 <muddamwarshubham98@gmail.com>
2022-10-24 16:40:39 +0530

    day3

---
+++ script/github.js
@@ -0,0 +1,8 @@
+GitHubCalendar(".calendar", "Shubham18598", { responsive: true });
+        
+// Use a proxy
+GitHubCalendar(".calendar", "Shubham18598", {
+    proxy(username) {
+        return fetch(`https://your-proxy.com/github?user=${username}`)
+    }
+})
